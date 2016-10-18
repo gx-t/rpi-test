7939c9525c45abfebc33b8c24a4c9137d16ddeb3
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2016-10-18 23:39:12 +0400

    Added 02-neon

---
+++ 02-neon.c
@@ -0,0 +1,63 @@
+// ./02-neon | aplay -t raw -c 2 -f FLOAT_LE -r 48000
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <arm_neon.h>
+
+#define PI		3.14159265358979323846
+
+#define FREQ_F32(freq_) (freq_ * 2 * PI / 48000)
+
+static int running = 1;
+
+#if 0
+int main() {
+	register float32x4_t p0 asm("q8") = {1.15, 1.16, 1.17, 1.21};
+	register float32x4_t p1 asm("q9") = {1.15, 1.16, 1.17, 1.21};
+	register float32x4_t p2 asm("q10");
+	asm("vadd.f32 q10, q9, q8");
+	asm("vadd.f32 q10, q9, q8");
+	asm("vadd.f32 q10, q9, q8");
+	asm("vadd.f32 q10, q9, q8");
+	asm("vadd.f32 q10, q9, q8");
+//	float32_t p3 = vaddvq_f32(p0);
+
+
+
+	return 0;
+}
+#endif
+
+void ctrl_c(int sig) {
+	running = 0;
+}
+
+int main() {
+	signal(SIGINT, ctrl_c);
+    float c0 = 1.0, s0 = 0.0, f0 = FREQ_F32(150);
+    float c1 = 1.0, s1 = 0.0, f1 = FREQ_F32(157);
+    float buff[4800 * 2];
+    float *p, *end = &buff[4800 * 2];
+
+    while(running) {
+        p = buff;
+
+        while(p < end) {
+
+            c0 -= s0 * f0;
+            s0 += c0 * f0;
+
+            c1 -= s1 * f1;
+            s1 += c1 * f1;
+
+            *p ++ = s0;
+            *p ++ = s1;
+
+        }
+		write(1, buff, sizeof(buff));
+    }
+    return 0;
+}
+
+
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all-deb: 01-deb
+all-deb: 00-deb 01-deb
 
 all-rel: 00-rel 01-rel
 
@@ -14,5 +14,11 @@ all-rel: 00-rel 01-rel
 	gcc -Wall -O2 -s -o 01-keyboard 01-keyboard.c
 
 
+02-deb:
+	gcc -Wall -g -march=armv8-a+crc -mfpu=neon -o 02-neon 02-neon.c
+02-rel:
+	gcc -Wall -O2 -march=armv8-a+crc -mfpu=neon -s -o 02-neon 02-neon.c
+
+
 clean:
-	rm -rf 00-mouse 01-keyboard
+	rm -rf 00-mouse 01-keyboard 02-neon
