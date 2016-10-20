243985a9d1586f63ba92e88615bd576452f70d24
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2016-10-20 22:04:36 +0400

    Moved to neon intrstics

--- 02-neon.c
+++ 02-neon.c
@@ -12,53 +12,34 @@
 
 static int running = 1;
 
-#if 0
-int main() {
-	register float32x4_t p0 asm("q8") = {1.15, 1.16, 1.17, 1.21};
-	register float32x4_t p1 asm("q9") = {1.15, 1.16, 1.17, 1.21};
-	register float32x4_t p2 asm("q10");
-	asm("vadd.f32 q10, q9, q8");
-	asm("vadd.f32 q10, q9, q8");
-	asm("vadd.f32 q10, q9, q8");
-	asm("vadd.f32 q10, q9, q8");
-	asm("vadd.f32 q10, q9, q8");
-	//	float32_t p3 = vaddvq_f32(p0);
-
-
-
-	return 0;
-}
-#endif
-
 void ctrl_c(int sig) {
+	signal(SIGINT, ctrl_c);
 	running = 0;
 }
 
 int main() {
+	
 	signal(SIGINT, ctrl_c);
-	float c0 = 1.0, s0 = 0.0, f0 = FREQ_F32(150);
-	float c1 = 1.0, s1 = 0.0, f1 = FREQ_F32(157);
-	float buff[4800 * 2];
-	float *p, *end = &buff[4800 * 2];
+
+	float32x2_t c = {1.0, 1.0};
+	float32x2_t s = {0.0, 0.0};
+	float32x2_t f = {FREQ_F32(150), FREQ_F32(155)};
+	
+	float32x2_t buff[4800];
+	float32x2_t *p, *end = &buff[4800];
 
 	while(running) {
 		p = buff;
 
 		while(p < end) {
 
-			c0 -= s0 * f0;
-			s0 += c0 * f0;
+			c = vmls_f32(c, s, f);
+			s = vmla_f32(s, c, f);
 
-			c1 -= s1 * f1;
-			s1 += c1 * f1;
-
-			*p ++ = s0;
-			*p ++ = s1;
-
+			*p ++ = s;
 		}
 		write(1, buff, sizeof(buff));
 	}
 	return 0;
 }
-
 
