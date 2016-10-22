2f0f091e0a0a126e7813cf416117c26a1baed5cc
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2016-10-22 22:05:42 +0400

    Added 03-camera

---
+++ 03-camera.c
@@ -0,0 +1,174 @@
+// !VoCam264
+// https://gist.github.com/maxlapshin/1253534
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <linux/videodev2.h>
+#include <stdint.h>
+#include <signal.h>
+
+#define BUFF_COUNT		4
+
+struct {
+	void* data;
+	size_t len;
+} static buff[BUFF_COUNT] = {{0}};
+
+static int fd = 0;
+static int running = 1;
+
+static const char* dev_name = "/dev/video0";
+
+static int set_fmt() {
+	struct v4l2_format fmt = {
+		.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+		.fmt.pix.width = 1920,
+		.fmt.pix.height = 1080
+	};
+
+	if(ioctl(fd, VIDIOC_S_FMT, &fmt)) {
+		perror("VIDIOC_S_FMT");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int req_buff() {
+	struct v4l2_requestbuffers req = {
+		.count = BUFF_COUNT,
+		.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+		.memory = V4L2_MEMORY_MMAP
+	};
+
+	if(ioctl(fd, VIDIOC_REQBUFS, &req)) {
+		perror("VIDIOC_REQBUFS");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int query_buf() {
+	uint32_t i;
+
+	for(i = 0; i < BUFF_COUNT; i ++) {
+		struct v4l2_buffer buf = {
+			.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+			.memory = V4L2_MEMORY_MMAP,
+			.index = i
+		};
+
+		if(ioctl(fd, VIDIOC_QUERYBUF, &buf)) {
+			perror("VIDIOC_QUERYBUF");
+			return -1;
+		}
+
+		buff[i].data = mmap(0, buf.length, PROT_READ, MAP_SHARED, fd, buf.m.offset);
+		if(!buff[i].data) {
+			perror("mmap");
+			return -1;
+		}
+		buff[i].len = buf.length;
+	}
+
+	return 0;
+}
+
+static void unmap_dev_mem() {
+	uint32_t i;
+
+	for(i = 0; i < BUFF_COUNT; i ++) {
+		if(!buff[i].len) {
+			continue;
+		}
+
+		munmap(buff[i].data, buff[i].len);
+	}
+}
+
+static int start_capture() {
+	uint32_t i;
+
+	for(i = 0; i < BUFF_COUNT; i ++) {
+		struct v4l2_buffer buf = {
+			.index = i,
+			.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+			.memory = V4L2_MEMORY_MMAP
+		};
+
+		if(ioctl(fd, VIDIOC_QBUF, &buf)) {
+			perror("VIDIOC_QBUF");
+			return -1;
+		}
+	}
+
+	enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+	if(ioctl(fd, VIDIOC_STREAMON, &type)) {
+		perror("VIDIOC_STREAMON");
+		return -1;
+	}
+	return 0;
+}
+
+static int stop_capture() {
+	enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+	if(ioctl(fd, VIDIOC_STREAMOFF, &type)) {
+		perror("VIDIOC_STREAMOFF");
+		return -1;
+	}
+	return 0;
+}
+
+static int read_block() {
+	struct v4l2_buffer buf = {
+		.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
+		.memory = V4L2_MEMORY_MMAP
+	};
+
+	if(ioctl(fd, VIDIOC_DQBUF, &buf)) {
+		perror("VIDIOC_DQBUF");
+		return -1;
+	}
+
+	(void)write(1, buff[buf.index].data, buff[buf.index].len);
+
+	if(ioctl(fd, VIDIOC_QBUF, &buf)) {
+		perror("VIDIOC_QBUF");
+		return -1;
+	}
+	return 0;
+}
+
+static void ctrl_c(int sig) {
+	signal(SIGINT, ctrl_c);
+	running = 0;
+}
+
+int main() {
+	signal(SIGINT, ctrl_c);
+	fd = open(dev_name, O_RDWR);
+
+	if(fd < 0) {
+		perror(dev_name);
+		return 1;
+	}
+	if(set_fmt() || req_buff() || query_buf()) {
+		close(fd);
+		return 2;
+	}
+	if(!start_capture()) {
+		while(running && !read_block());
+		stop_capture();
+	}
+	unmap_dev_mem();
+	close(fd);
+	fprintf(stderr, "\n.END\n");
+	return 0;
+}
+
--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-deb: 00-deb 01-deb 02-deb
+deb: 00-deb 01-deb 02-deb 03-deb
 
-rel: 00-rel 01-rel 02-rel
+rel: 00-rel 01-rel 02-rel 03-rel
 
 00-deb:
 	gcc -Wall -g -o 00-mouse 00-mouse.c
@@ -20,8 +20,14 @@ rel: 00-rel 01-rel 02-rel
 	gcc -Wall -O3 -s -mcpu=cortex-a53 -mfpu=neon -o 02-neon 02-neon.c
 
 
+03-deb:
+	gcc -Wall -g -mcpu=cortex-a53 -mfpu=neon -o 03-camera 03-camera.c
+03-rel:
+	gcc -Wall -O3 -s -mcpu=cortex-a53 -mfpu=neon -o 03-camera 03-camera.c
+
+
 clean:
-	rm -rf 00-mouse 01-keyboard 02-neon
+	rm -rf 00-mouse 01-keyboard 02-neon 03-camera
 
 asm:
 	gcc -c -g -mcpu=cortex-a53 -mfpu=neon 02-neon.c -Wa,-a,-ad -O3 > 02-neon-4.asm
