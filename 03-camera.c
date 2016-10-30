8598e166dc8e8229c38df3b3f7dbaeb02ee5294f
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2016-10-31 00:44:53 +0400

    Added TCP streaming

--- 03-camera.c
+++ 03-camera.c
@@ -8,9 +8,12 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <linux/videodev2.h>
 #include <stdint.h>
 #include <signal.h>
+#include <time.h>
 
 #define BUFF_COUNT		4
 
@@ -21,8 +24,9 @@ struct {
 
 static int fd = 0;
 static int running = 1;
+static int ss = -1, cc = 1;
 
-static const char* dev_name = "/dev/video1";
+static const char* dev_name = "/dev/video0";
 
 static int set_fmt() {
 	struct v4l2_format fmt = {
@@ -127,6 +131,11 @@ static int stop_capture() {
 	return 0;
 }
 
+static const char* curr_time() {
+	time_t tt = time(0);
+	return asctime(localtime(&tt));
+}
+
 static int handle_block() {
 	struct v4l2_buffer buf = {
 		.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
@@ -138,39 +147,143 @@ static int handle_block() {
 		return -1;
 	}
 
-	(void)write(1, buff[buf.index].data, buff[buf.index].len);
+	int res = buff[buf.index].len == write(cc, buff[buf.index].data, buff[buf.index].len);
+	res = !res;
 
 	if(ioctl(fd, VIDIOC_QBUF, &buf)) {
 		perror("VIDIOC_QBUF");
 		return -1;
 	}
-	return 0;
+
+	if(res && (-1 != cc)) {
+		close(cc);
+		cc = -1;
+		fcntl(ss, F_SETFL, fcntl(ss, F_GETFL, 0) & ~O_NONBLOCK);
+		fprintf(stderr, "%s\tDisconnected\n", curr_time());
+	}
+	return res;
 }
 
 static void ctrl_c(int sig) {
 	signal(SIGINT, ctrl_c);
+
 	running = 0;
+
+	if(-1 != ss) {
+		close(ss);
+		ss = -1;
+	}
+
+	if(-1 != cc) {
+		close(cc);
+		cc = -1;
+	}
 }
 
-int main() {
-	signal(SIGINT, ctrl_c);
+static void set_conn_timeout() {
+	struct timeval tt = {
+		.tv_sec = 1,
+		.tv_usec = 0
+	};      
+	setsockopt(cc, SOL_SOCKET, SO_SNDTIMEO, (char*)&tt, sizeof(tt));
+}
+
+static int update_conn() {
+	struct sockaddr_in sin = {0};
+	socklen_t addr_len = sizeof(sin);
+
+	int new_conn = accept(ss, (struct sockaddr*)&sin, &addr_len);
+
+	if(-1 == new_conn) {
+		return 0;
+	}
+
+	fprintf(stderr, "%s\tUpdated to: %s:%d\n", curr_time(), inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
+
+	set_conn_timeout();
+
+	return cc != dup2(new_conn, cc);
+}
+
+static int run_camera() {
 	fd = open(dev_name, O_RDWR);
 
-	if(fd < 0) {
+	if(-1 == fd) {
 		perror(dev_name);
 		return 1;
 	}
+
 	if(set_fmt() || req_buff() || map_device()) {
 		close(fd);
 		return 2;
 	}
+
 	if(!start_capture()) {
-		while(running && !handle_block());
+		while(running && !handle_block() && !update_conn());
 		stop_capture();
 	}
+
 	unmap_device();
 	close(fd);
-	fprintf(stderr, "\n.END\n");
+
 	return 0;
+}
+
+static int start_server() {
+	ss = socket(AF_INET, SOCK_STREAM, 0);
+	if(-1 == ss) {
+		perror("socket");
+		return 3;
+	}
+	struct sockaddr_in sin = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(8888)
+	};
+	int on = 1;
+	setsockopt(ss, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
+	if(-1 == bind(ss, (struct sockaddr *) &sin, sizeof(sin))) {
+		perror("bind");
+		return 4;
+	}
+	if(-1 == listen(ss, 1)) {
+		perror("listen");
+		return 5;
+	}
+	return 0;
+}
+
+static int wait_client() {
+	struct sockaddr_in sin = {0};
+	socklen_t addr_len = sizeof(sin);
+	cc = accept(ss, (struct sockaddr*)&sin, &addr_len);
+	if(-1 == cc) {
+		if(ss != -1) {
+			perror("accept");
+		}
+		return 6;
+	}
+	fcntl(ss, F_SETFL, fcntl(ss, F_GETFL, 0) | O_NONBLOCK);
+	set_conn_timeout();
+	fprintf(stderr, "%s\tConnected: %s:%d\n", curr_time(), inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
+	return 0;
+}
+
+static void stop_server() {
+	if(-1 != ss) {
+		close(ss);
+	}
+}
+
+int main() {
+	signal(SIGINT, ctrl_c);
+	int res = start_server();
+	if(res) {
+		return res;
+	}
+	while(!(res = wait_client()) && !(res = run_camera()));
+	stop_server();
+	fprintf(stderr, "\n.END\n");
+	return res;
 }
 
