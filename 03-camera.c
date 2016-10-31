94806df18e76315bc3e068799b6fca600067ea27
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2016-11-01 00:07:38 +0400

    Separate exec based streamer

--- 03-camera.c
+++ 03-camera.c
@@ -8,12 +8,9 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <linux/videodev2.h>
 #include <stdint.h>
 #include <signal.h>
-#include <time.h>
 
 #define BUFF_COUNT		4
 
@@ -24,9 +21,6 @@ struct {
 
 static int fd = 0;
 static int running = 1;
-static int ss = -1, cc = 1;
-
-static const char* dev_name = "/dev/video0";
 
 static int set_fmt() {
 	struct v4l2_format fmt = {
@@ -131,11 +125,6 @@ static int stop_capture() {
 	return 0;
 }
 
-static const char* curr_time() {
-	time_t tt = time(0);
-	return asctime(localtime(&tt));
-}
-
 static int handle_block() {
 	struct v4l2_buffer buf = {
 		.type = V4L2_BUF_TYPE_VIDEO_CAPTURE,
@@ -147,143 +136,43 @@ static int handle_block() {
 		return -1;
 	}
 
-	int res = buff[buf.index].len == write(cc, buff[buf.index].data, buff[buf.index].len);
+	int res = buff[buf.index].len == write(STDOUT_FILENO, buff[buf.index].data, buff[buf.index].len);
 	res = !res;
 
 	if(ioctl(fd, VIDIOC_QBUF, &buf)) {
 		perror("VIDIOC_QBUF");
 		return -1;
 	}
-
-	if(res && (-1 != cc)) {
-		close(cc);
-		cc = -1;
-		fcntl(ss, F_SETFL, fcntl(ss, F_GETFL, 0) & ~O_NONBLOCK);
-		fprintf(stderr, "%s\tDisconnected\n", curr_time());
-	}
 	return res;
 }
 
 static void ctrl_c(int sig) {
 	signal(SIGINT, ctrl_c);
-
 	running = 0;
-
-	if(-1 != ss) {
-		close(ss);
-		ss = -1;
-	}
-
-	if(-1 != cc) {
-		close(cc);
-		cc = -1;
-	}
 }
 
-static void set_conn_timeout() {
-	struct timeval tt = {
-		.tv_sec = 1,
-		.tv_usec = 0
-	};      
-	setsockopt(cc, SOL_SOCKET, SO_SNDTIMEO, (char*)&tt, sizeof(tt));
-}
-
-static int update_conn() {
-	struct sockaddr_in sin = {0};
-	socklen_t addr_len = sizeof(sin);
-
-	int new_conn = accept(ss, (struct sockaddr*)&sin, &addr_len);
-
-	if(-1 == new_conn) {
-		return 0;
-	}
-
-	fprintf(stderr, "%s\tUpdated to: %s:%d\n", curr_time(), inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
-
-	set_conn_timeout();
-
-	return cc != dup2(new_conn, cc);
-}
-
-static int run_camera() {
-	fd = open(dev_name, O_RDWR);
-
-	if(-1 == fd) {
-		perror(dev_name);
+int main(int argc, char* argv[]) {
+	if(argc != 2) {
 		return 1;
 	}
+	signal(SIGINT, ctrl_c);
+	fd = open(argv[1], O_RDWR);
 
-	if(set_fmt() || req_buff() || map_device()) {
-		close(fd);
+	if(fd < 0) {
+		perror(argv[1]);
 		return 2;
 	}
-
-	if(!start_capture()) {
-		while(running && !handle_block() && !update_conn());
-		stop_capture();
-	}
-
-	unmap_device();
-	close(fd);
-
-	return 0;
-}
-
-static int start_server() {
-	ss = socket(AF_INET, SOCK_STREAM, 0);
-	if(-1 == ss) {
-		perror("socket");
+	if(set_fmt() || req_buff() || map_device()) {
+		close(fd);
 		return 3;
 	}
-	struct sockaddr_in sin = {
-		.sin_family = AF_INET,
-		.sin_addr.s_addr = INADDR_ANY,
-		.sin_port = htons(8888)
-	};
-	int on = 1;
-	setsockopt(ss, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
-	if(-1 == bind(ss, (struct sockaddr *) &sin, sizeof(sin))) {
-		perror("bind");
-		return 4;
+	if(!start_capture()) {
+		while(running && !handle_block());
+		stop_capture();
 	}
-	if(-1 == listen(ss, 1)) {
-		perror("listen");
-		return 5;
-	}
-	return 0;
-}
-
-static int wait_client() {
-	struct sockaddr_in sin = {0};
-	socklen_t addr_len = sizeof(sin);
-	cc = accept(ss, (struct sockaddr*)&sin, &addr_len);
-	if(-1 == cc) {
-		if(ss != -1) {
-			perror("accept");
-		}
-		return 6;
-	}
-	fcntl(ss, F_SETFL, fcntl(ss, F_GETFL, 0) | O_NONBLOCK);
-	set_conn_timeout();
-	fprintf(stderr, "%s\tConnected: %s:%d\n", curr_time(), inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
-	return 0;
-}
-
-static void stop_server() {
-	if(-1 != ss) {
-		close(ss);
-	}
-}
-
-int main() {
-	signal(SIGINT, ctrl_c);
-	int res = start_server();
-	if(res) {
-		return res;
-	}
-	while(!(res = wait_client()) && !(res = run_camera()));
-	stop_server();
+	unmap_device();
+	close(fd);
 	fprintf(stderr, "\n.END\n");
-	return res;
+	return 0;
 }
 
--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-deb: 00-deb 01-deb 02-deb 03-deb
+deb: 00-deb 01-deb 02-deb 03-deb 04-deb
 
-rel: 00-rel 01-rel 02-rel 03-rel
+rel: 00-rel 01-rel 02-rel 03-rel 04-rel
 
 00-deb:
 	gcc -Wall -g -o 00-mouse 00-mouse.c
@@ -26,8 +26,14 @@ rel: 00-rel 01-rel 02-rel 03-rel
 	gcc -Wall -O3 -s -mcpu=cortex-a53 -mfpu=neon -o 03-camera 03-camera.c
 
 
+04-deb:
+	gcc -Wall -g -mcpu=cortex-a53 -mfpu=neon -o 04-stream 04-stream.c
+04-rel:
+	gcc -Wall -O3 -s -mcpu=cortex-a53 -mfpu=neon -o 04-stream 04-stream.c
+
+
 clean:
-	rm -rf 00-mouse 01-keyboard 02-neon 03-camera
+	rm -rf 00-mouse 01-keyboard 02-neon 03-camera 04-stream
 
 asm:
 	gcc -c -g -mcpu=cortex-a53 -mfpu=neon 02-neon.c -Wa,-a,-ad -O3 > 02-neon.asm
