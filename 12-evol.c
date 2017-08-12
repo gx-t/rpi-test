f03488ec640be48616cf49ab361ba1dfe41ab8b6
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2017-08-12 14:39:41 +0400

    inputs as command arguments

--- 12-evol.c
+++ 12-evol.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 static void evol_quad_eq(float a, float b, float c, unsigned gen_count, unsigned pop_count, float min, float max, float mut)
 {
-	int cmp_proc(const void* x1, const void* x2) {
+	float pop[pop_count * 2];
+	
+	int select_proc(const void* x1, const void* x2)
+	{
 		float v1 = a * *(float*)x1 * *(float*)x1 + b * *(float*)x1 + c;
 		float v2 = a * *(float*)x2 * *(float*)x2 + b * *(float*)x2 + c;
 		v1 *= v1;
@@ -18,31 +22,90 @@ static void evol_quad_eq(float a, float b, float c, unsigned gen_count, unsigned
 		return 0;
 	}
 
-	float pop[pop_count * 2];
-	unsigned i;
-
-	srand48(time(0));
-	for(i = 0; i < pop_count; i ++) {
-		pop[i] = min + (max - min) * drand48();
+	void fill()
+	{
+		float step = (max - min) / pop_count;
+		float val = min;
+		unsigned i;
+		for(i = 0; i < pop_count; i ++, val += step) {
+			pop[i] = val;
+		}
 	}
 
-	while(gen_count --) {
+	void reproduce_mutate()
+	{
+		unsigned i;
 		for(i = 0; i < pop_count; i ++) {
 			pop[i + pop_count] = pop[i] - mut + mut * 2.0 * drand48();
 		}
-		qsort(pop, pop_count * 2, sizeof(pop[0]), cmp_proc);
 	}
 
-	for(i = 0; i < pop_count; i ++) {
-		printf("%g ", pop[i]);
+	void print() {
+		unsigned i;
+		for(i = 0; i < pop_count; i ++) {
+			printf("%g ", pop[i]);
+		}
+		printf("\n");
 	}
-	printf("\n");
+
+	void sort_select()
+	{
+		qsort(pop, pop_count * 2, sizeof(pop[0]), select_proc);
+	}
+
+
+	fill();
+
+	srand48(time(0));
+
+	while(gen_count --) {
+		reproduce_mutate();
+		sort_select();
+	}
+
+	print();
 }
 
-int main()
-{
-	evol_quad_eq(2, -4, -2, 256, 256, -100.0, 100.0, 1.0);
+static int cmd_evol_quad_eq(int argc, char* argv[]) {
+	if(argc != 9) {
+		fprintf(stderr, "Usage: %s a, b, c, <generation count> <poplation count> <min value> <max value> <mutation>\n", argv[0]);
+		fprintf(stderr, "Example: 2 -4 -2 256 256 -100.0 100.0 1.0\n");
+		return 3;
+	}
+	argc --;
+	argv ++;
+
+	float a, b, c, min, max, mut;
+	unsigned gen_count, pop_count;
+	if(1 != sscanf(argv[0], "%g", &a)
+	|| 1 != sscanf(argv[1], "%g", &b)
+	|| 1 != sscanf(argv[2], "%g", &c)
+	|| 1 != sscanf(argv[3], "%u", &gen_count)
+	|| 1 != sscanf(argv[4], "%u", &pop_count)
+	|| 1 != sscanf(argv[5], "%g", &min)
+	|| 1 != sscanf(argv[6], "%g", &max)
+	|| 1 != sscanf(argv[7], "%g", &mut)) {
+		fprintf(stderr, "Invalid value\n");
+		return 4;
+	}
+	evol_quad_eq(a, b, c, gen_count, pop_count, min, max, mut);
 	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc < 2) {
+		fprintf(stderr, "Usage: %s <subcommand> <arguments>\n", argv[0]);
+		fprintf(stderr, "Subcommands:\n");
+		fprintf(stderr, "\tevol-quad-eq\n");
+		return 1;
+	}
+	argc --;
+	argv ++;
+	if(!strcmp(*argv, "evol-quad-eq"))
+		return cmd_evol_quad_eq(argc, argv);
+	fprintf(stderr, "Unknown subcommand: %s\n", *argv);
+	return 2;
 }
 
 
