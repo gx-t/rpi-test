c03fd51549290d5d46ad52725bf4f05d2650343b
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2018-03-28 15:45:05 +0400

    globals->pass through args

--- 06-fb.c
+++ 06-fb.c
@@ -11,7 +11,7 @@
 
 #define BYTES_PER_PIXEL     4
 
-struct {
+struct ALL {
     struct {
         uint32_t xoffset
             , yoffset
@@ -50,32 +50,33 @@ struct {
         } color;
     } board;
 
-    volatile int32_t running;
-}static g;
+};
+
+volatile int32_t running;
 
 #define BOARD_LEFT      300
 #define BOARD_TOP       300
 #define BOARD_WIDTH     300
 #define BOARD_HEIGHT    200
 
-static void draw_borders(uint32_t* pp, int x, int y)
+static void draw_borders(struct ALL* all, uint32_t* pp, int x, int y)
 {
     uint32_t r = BOARD_WIDTH - 1
         , b = BOARD_HEIGHT - 1;
 
     if(0 == x || r == x || 0 == y || b == y) {
-        *pp = g.board.color.border;
+        *pp = all->board.color.border;
     }
 }
 
-static void process_ball_collision(int32_t cx, int32_t cy)
+static void process_ball_collision(struct ALL* all, int32_t cx, int32_t cy)
 {
-    g.board.ball.vx = -g.board.ball.vx;
-    g.board.ball.vy = -g.board.ball.vy;
+    all->board.ball.vx = -all->board.ball.vx;
+    all->board.ball.vy = -all->board.ball.vy;
     if(0 == cx) {
-        g.board.ball.vx = -g.board.ball.vx;
+        all->board.ball.vx = -all->board.ball.vx;
     } else if(0 == cy) {
-        g.board.ball.vy = -g.board.ball.vy;
+        all->board.ball.vy = -all->board.ball.vy;
     }
 }
 
@@ -89,48 +90,48 @@ static void draw_line(uint32_t* pp, int x, int y, int32_t cx, int32_t cy, int32_
     *pp = color;
 }
 
-static void draw_walls(uint32_t* pp, int x, int y)
+static void draw_walls(struct ALL* all, uint32_t* pp, int x, int y)
 {
-    int32_t sy = 4 * g.board.gs;
+    int32_t sy = 4 * all->board.gs;
     int32_t cy = sy;
-    draw_line(pp, x, y, g.board.wall.x0, cy, g.board.wall.length, 0x00FF0000);
+    draw_line(pp, x, y, all->board.wall.x0, cy, all->board.wall.length, 0x00FF0000);
     cy += sy;
-    draw_line(pp, x, y, g.board.wall.x1, cy, g.board.wall.length, 0x0000FF00);
+    draw_line(pp, x, y, all->board.wall.x1, cy, all->board.wall.length, 0x0000FF00);
     cy += sy;
-    draw_line(pp, x, y, g.board.wall.x2, cy, g.board.wall.length, 0x000000FF);
+    draw_line(pp, x, y, all->board.wall.x2, cy, all->board.wall.length, 0x000000FF);
     cy += sy;
-    draw_line(pp, x, y, g.board.wall.x3, cy, g.board.wall.length, 0x0000FFFF);
+    draw_line(pp, x, y, all->board.wall.x3, cy, all->board.wall.length, 0x0000FFFF);
 }
 
-static void draw_ball(uint32_t* pp, int x, int y)
+static void draw_ball(struct ALL* all, uint32_t* pp, int x, int y)
 {
-    x -= (int)g.board.ball.cx;
-    y -= (int)g.board.ball.cy;
-    int r = (int)g.board.ball.rad;
+    x -= (int)all->board.ball.cx;
+    y -= (int)all->board.ball.cy;
+    int r = (int)all->board.ball.rad;
 
     if(x * x + y * y < r * r) {
-        if(*pp != g.board.color.bground)
-            process_ball_collision(x, y);
+        if(*pp != all->board.color.bground)
+            process_ball_collision(all, x, y);
 
-        *pp = g.board.color.ball;
+        *pp = all->board.color.ball;
     }
 }
 
-static void update_ball_moving()
+static void update_ball_moving(struct ALL* all)
 {
-    g.board.ball.cx += g.board.ball.vx;
-    g.board.ball.cy += g.board.ball.vy;
+    all->board.ball.cx += all->board.ball.vx;
+    all->board.ball.cy += all->board.ball.vy;
 }
 
 //-----------------------------------------------------------------------------
 
-static int init_fb()
+static int init_fb(struct ALL* all)
 {
     int ret = 0;
     struct fb_var_screeninfo vinfo;
     struct fb_fix_screeninfo finfo;
 
-    g.screen.fd = -1;
+    all->screen.fd = -1;
 
     int fd = open("/dev/fb0", O_RDWR);
     if (fd == -1) {
@@ -154,122 +155,123 @@ static int init_fb()
         fprintf(stderr, "Unsupported bits per pixel: %u. Only %d bpp is supported\n", vinfo.bits_per_pixel, BYTES_PER_PIXEL * 8);
         return 4;
     }
-    g.screen.xoffset = vinfo.xoffset;
-    g.screen.yoffset = vinfo.yoffset;
-    g.screen.line_length = finfo.line_length;
+    all->screen.xoffset = vinfo.xoffset;
+    all->screen.yoffset = vinfo.yoffset;
+    all->screen.line_length = finfo.line_length;
 
     printf("%dx%d\n", vinfo.xres, vinfo.yres);
 
-    g.screen.size = vinfo.xres * vinfo.yres * BYTES_PER_PIXEL;
+    all->screen.size = vinfo.xres * vinfo.yres * BYTES_PER_PIXEL;
 
-    g.screen.fbp = (unsigned char *)mmap(0, g.screen.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    all->screen.fbp = (unsigned char *)mmap(0, all->screen.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
 
-    if ((int)g.screen.fbp == -1) {
+    if ((int)all->screen.fbp == -1) {
         close(fd);
         perror("Error: failed to map framebuffer device to memory");
         return 5;
     }
 
-    g.screen.fd = fd;
+    all->screen.fd = fd;
     return ret;
 }
 
-static void free_fb()
+static void free_fb(struct ALL* all)
 {
-    close(g.screen.fd);
-    munmap(g.screen.fbp, g.screen.size);
+    close(all->screen.fd);
+    munmap(all->screen.fbp, all->screen.size);
 }
 
 //-----------------------------------------------------------------------------
 
-static void init_ball()
+static void init_ball(struct ALL* all)
 {
-    g.board.ball.rad = g.board.gs;
-    g.board.ball.cx = BOARD_WIDTH / 2;
-    g.board.ball.cy = BOARD_HEIGHT / 2;
-    g.board.ball.vx = -2;
-    g.board.ball.vy = -3;
+    all->board.ball.rad = all->board.gs;
+    all->board.ball.cx = BOARD_WIDTH / 2;
+    all->board.ball.cy = BOARD_HEIGHT / 2;
+    all->board.ball.vx = -2;
+    all->board.ball.vy = -3;
 }
 
-static void init_color()
+static void init_color(struct ALL* all)
 {
-    g.board.color.bground	= 0x00000000;
-    g.board.color.border	= 0x00FF8888;
-    g.board.color.ball		= 0x00FF0000;
+    all->board.color.bground	= 0x00000000;
+    all->board.color.border	= 0x00FF8888;
+    all->board.color.ball		= 0x00FF0000;
 }
 
-static void init_wall()
+static void init_wall(struct ALL* all)
 {
     srand(time(0));
-    g.board.wall.x0 = 1 + rand() % (20 * g.board.gs - 2);
-    g.board.wall.x1 = 1 + rand() % (20 * g.board.gs - 2);
-    g.board.wall.x2 = 1 + rand() % (20 * g.board.gs - 2);
-    g.board.wall.x3 = 1 + rand() % (20 * g.board.gs - 2);
+    all->board.wall.x0 = 1 + rand() % (20 * all->board.gs - 2);
+    all->board.wall.x1 = 1 + rand() % (20 * all->board.gs - 2);
+    all->board.wall.x2 = 1 + rand() % (20 * all->board.gs - 2);
+    all->board.wall.x3 = 1 + rand() % (20 * all->board.gs - 2);
 
-    g.board.wall.length = 10 * g.board.gs;
+    all->board.wall.length = 10 * all->board.gs;
 }
 
-static void init_board()
+static void init_board(struct ALL* all)
 {
-    g.board.gs		= 10;
+    all->board.gs		= 10;
 
-    init_ball();
-    init_color();
-    init_wall();
+    init_ball(all);
+    init_color(all);
+    init_wall(all);
 }
 
-static void update_board()
+static void update_board(struct ALL* all)
 {
-    update_ball_moving();
+    update_ball_moving(all);
 }
 
-static void scan_write_fb()
+static void scan_write_fb(struct ALL* all)
 {
     int x, y;
-    uint32_t* pp = (uint32_t*)g.screen.fbp;
-    pp += (BOARD_LEFT + g.screen.xoffset);
-    pp += (BOARD_TOP + g.screen.yoffset) * g.screen.line_length / BYTES_PER_PIXEL;
+    uint32_t* pp = (uint32_t*)all->screen.fbp;
+    pp += (BOARD_LEFT + all->screen.xoffset);
+    pp += (BOARD_TOP + all->screen.yoffset) * all->screen.line_length / BYTES_PER_PIXEL;
 
-    for(y = 0; y < BOARD_HEIGHT; y++, pp += (g.screen.line_length / BYTES_PER_PIXEL - BOARD_WIDTH)) {
+    for(y = 0; y < BOARD_HEIGHT; y++, pp += (all->screen.line_length / BYTES_PER_PIXEL - BOARD_WIDTH)) {
         for(x = 0; x < BOARD_WIDTH; x++, pp ++) {
-            *pp = g.board.color.bground;
-            draw_borders(pp, x, y);
-            draw_walls(pp, x, y);
-            draw_ball(pp, x, y);
+            *pp = all->board.color.bground;
+            draw_borders(all, pp, x, y);
+            draw_walls(all, pp, x, y);
+            draw_ball(all, pp, x, y);
         }
     }
 }
 
 static void ctrl_c(int sig) {
     signal(SIGINT, ctrl_c);
-    g.running = 0;
-    printf("CTRL+C %d\n", g.running);
+    running = 0;
+    printf("CTRL+C %d\n", running);
 }
 
 int main()
 {
+    struct ALL all;
     int ret = 0;
 
-    g.running = 1;
+    running = 1;
     signal(SIGINT, ctrl_c);
 
-    if((ret = init_fb()))
+    if((ret = init_fb(&all)))
         return ret;
 
-    init_board();
+    init_board(&all);
 
-    while(g.running) {
+    while(running) {
         uint32_t count = 300;
-        while(g.running && count --) {
-            update_board();
-            scan_write_fb();
-            ioctl(g.screen.fd, FBIO_WAITFORVSYNC, 0);
+        while(running && count --) {
+            update_board(&all);
+            ioctl(all.screen.fd, FBIO_WAITFORVSYNC, 0);
+            scan_write_fb(&all);
         }
-        init_wall();
+        init_wall(&all);
     }
 
-    free_fb();
+    free_fb(&all);
     return ret;
 }
 
