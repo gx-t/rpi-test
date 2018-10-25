ac9013fce08d43249ac390d18e987c019cf544ee
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2018-10-25 16:00:37 +0400

    Finished receive loop, not working

--- 15-udp.c
+++ 15-udp.c
@@ -1,34 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <signal.h>
 #include <sys/wait.h>
-
-/*
-
-static int arg_ip(const char* ip_str, struct sockaddr_in* addr)
-{
-    struct hostent* he = gethostbyname(ip_str);
-    if(!he) {
-        perror(ip_str);
-        return show_usage(4);
-    }
-
-    memcpy(&addr->sin_addr, he->h_addr_list[0], sizeof(addr->sin_addr));
-
-    return 0;
-}
-static int client_send_reg(struct sockaddr_in* addr, int id)
-{
-    uint8_t buff[2] = {CMD_REG, id};
-    if(sizeof(buff) != sendto(g_sock, buff, sizeof(buff), 0, (struct sockaddr *)addr, sizeof(struct sockaddr_in))) {
-        perror("sendto");
-        return 6;
-    }
-    return 0;
-}
-*/
+#include <string.h>
 
 static void ctrl_c(int sig)
 {
@@ -40,10 +18,38 @@ static int send_loop()
 {
     char buff[256];
     while(fgets(buff, sizeof(buff), stdin)) {
-    }
-    fprintf(stderr, "End of send loop (%d)\n", getpid());
+        char* data = 0;
+        char* addr_str = strtok_r(buff, " \t", &data);
+        char* port_str = strtok_r(0, " \t", &data);
+        struct hostent* he = gethostbyname(addr_str);
 
+        if(!he) {
+            fprintf(stderr, "Cannot find host: %s\n", addr_str);
+            continue;
+        }
+
+        int port = atoi(port_str);
+        if(port < 1024 || port > 65535)
+            port = 23456;
+
+        struct sockaddr_in addr = {
+            .sin_family = AF_INET,
+            .sin_addr.s_addr = *(uint32_t*)he->h_addr_list[0],
+            .sin_port = htons(port)
+        };
+
+        int len = strlen(data);
+        if(len < 1)
+            continue;
+
+        if(len != sendto(1, data, len, 0, (struct sockaddr *)&addr, sizeof(addr)))
+            fprintf(stderr, "Error sending data\n");
+
+    }
+
+    fprintf(stderr, "End of send loop (%d)\n", getpid());
     wait(0);
+    fprintf(stderr, "After wait (%d)\n", getpid());
     return 0;
 }
 
@@ -62,7 +68,8 @@ static int recv_loop()
         return 3;
     }
     while(-1 != (len = recvfrom(0, buff, sizeof(buff), 0, (struct sockaddr*)&addr, &addr_len))) {
-        fprintf(stderr, ">> %d\n", getpid());
+        if(len != write(1, buff, len))
+            perror("Write stdout");
     }
 
     fprintf(stderr, "End of recv loop (%d)\n", getpid());
