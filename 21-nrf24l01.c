f47e0bd6b09622c90e08038ec5fd18f8c580f0ce
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2021-02-11 00:41:48 +0400

    renamed functions prefix

--- 21-nrf24l01.c
+++ 21-nrf24l01.c
@@ -1,5 +1,6 @@
 // http://shah32768.sdf.org/pdf/nRF24L01_Product_Specification_v2_0-9199.pdf
 // http://shah32768.sdf.org/pdf/nRF%20Performance%20Test%20Instructions.%20nRF24L01+.%20Application%20Note.pdf
+// http://shah32768.sdf.org/pdf/Application%20note,Nordic%20nRF24L01%20with%20Bascom-Avr.pdf
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,7 +22,7 @@ static int __argc__ = 0;
 static char** __argv__ = 0;
 static int spi = 0;
 
-static int nrf24l01_read_reg(uint8_t reg, uint8_t* val)
+static int nrf24_read_reg(uint8_t reg, uint8_t* val)
 {
     char rx[2] = {0, 0}, tx[2] = {reg, 0};
     if(0 > spiXfer(spi, tx, rx, 2)) {
@@ -32,7 +33,7 @@ static int nrf24l01_read_reg(uint8_t reg, uint8_t* val)
     return 0;
 }
 
-static int nrf24l01_write_reg(uint8_t reg, uint8_t val)
+static int nrf24_write_reg(uint8_t reg, uint8_t val)
 {
     char rx[2] = {0, 0}, tx[2] = {0x20 | (reg & 0x1F), val};
     if(0 > spiXfer(spi, tx, rx, 2)) {
@@ -42,13 +43,13 @@ static int nrf24l01_write_reg(uint8_t reg, uint8_t val)
     return 0;
 }
 
-static int nrf24l01_print_regs()
+static int nrf24_print_regs()
 {
     int res = 0;
     uint8_t reg = 0;
     for(reg = 0; !res && reg < 0x18; reg ++) {
         uint8_t val = 0;
-        if((res = nrf24l01_read_reg(reg, &val))) {
+        if((res = nrf24_read_reg(reg, &val))) {
             perror(__func__);
             break;
         }
@@ -70,11 +71,11 @@ static void show_usage()
 //based on "nRF Performance Test Instructions nRF24L01+ Application Note" => 6 Receiver sensitivity
 static int f_scan()
 {
-    nrf24l01_write_reg(0x00, 0x03); //power up, RX
+    nrf24_write_reg(0x00, 0x03); //power up, RX
     usleep(1500); //data sheet p.20, f.3
-    nrf24l01_write_reg(0x01, 0x00); //Disable all auto acknowledge
-    nrf24l01_write_reg(0x04, 0x00); //Disable all auto retransmit
-    nrf24l01_write_reg(0x06, 0b1001); //LNA on, 2Mbit/s
+    nrf24_write_reg(0x01, 0x00); //Disable all auto acknowledge
+    nrf24_write_reg(0x04, 0x00); //Disable all auto retransmit
+    nrf24_write_reg(0x06, 0b1001); //LNA on, 2Mbit/s
     uint32_t hist[128] = {0};
     uint8_t ch = 0;
     while(running) {
@@ -82,12 +83,12 @@ static int f_scan()
         printf(" 2400 Mhz | 2410 Mhz | 2420 Mhz | 2430 Mhz | 2440 Mhz | 2450 Mhz | 2460 Mhz | 2470 Mhz | 2480 Mhz | 2490 Mhz | 2500 Mhz | 2520 Mhz | 2520 Mhz \n");
         while(running && line --) {
             for(ch = 0; running && ch < 128; ch ++) {
-                nrf24l01_write_reg(0x05, ch); //set channel (data sheet page 54)
+                nrf24_write_reg(0x05, ch); //set channel (data sheet page 54)
                 gpioWrite(17, 1); //chip enable
                 usleep(500); //data sheet p.20, f.3
                 gpioWrite(17, 0); //chip disable
                 uint8_t cd = 0;
-                nrf24l01_read_reg(0x09, &cd); //carrier detect (data sheet page 55)
+                nrf24_read_reg(0x09, &cd); //carrier detect (data sheet page 55)
                 if(ch && !(ch % 10))
                     printf("|");
                 printf(cd & 1 ? "*" : " ");
@@ -96,6 +97,8 @@ static int f_scan()
             printf("\n");
         }
     }
+    gpioWrite(17, 0); //chip disable
+    nrf24_write_reg(0x00, 0x01); //power down, RX
     for(ch = 0; ch < 128; ch ++) {
         printf("Channel %u, %d Mhz -- %u\n", ch, 2400 + ch, hist[ch]);
     }
@@ -115,15 +118,16 @@ static int f_carrier()
     if(power < 0 || power > 3)
         power = 3;
     fprintf(stderr, "%s: using channel %d (%d Mhz) and power level %d\n", __argv__[1], ch, 2400 + ch, power);
-    nrf24l01_write_reg(0x00, 0x02); //power up, TX
+    nrf24_write_reg(0x00, 0x02); //power up, TX
     usleep(1500); //data sheet p.20, f.3
-    nrf24l01_write_reg(0x05, (uint8_t)ch); //set channel (data sheet page 54)
-    nrf24l01_write_reg(0x06, 0x90 | (power << 1)); //AN p.5. Bit 7 - "Cont wave" is not documented in datasheet.
+    nrf24_write_reg(0x05, (uint8_t)ch); //set channel (data sheet page 54)
+    nrf24_write_reg(0x06, 0x90 | (power << 1)); //AN p.5. Bit 7 - "Cont wave" is not documented in datasheet.
     gpioWrite(17, 1); //chip enable
     while(running) {
         pause();
     }
     gpioWrite(17, 0); //chip disable
+    nrf24_write_reg(0x00, 0x00); //power down, TX
     return 0;
 }
 
@@ -141,10 +145,10 @@ static int spi_op(int (*f)())
     gpioSetMode(17, PI_OUTPUT);
     gpioWrite(17, 0); //chip disable
     usleep(10300); //data sheet p.20, f.3
-    nrf24l01_print_regs();
+    nrf24_print_regs();
     int res = f();
-    nrf24l01_write_reg(0x00, 0x00); //power down
-    nrf24l01_print_regs();
+    nrf24_write_reg(0x00, 0x00); //power down
+    nrf24_print_regs();
     spiClose(spi);
     gpioTerminate();
     return res;
