9e13b83824a6d6120df2e768a365a83a1bafee09
Արտաշես Շահինյան <artashes.shahinyan@gmail.com>
2021-12-02 19:47:27 +0400

    preparation for parametrization

--- 22-temp-cgi.c
+++ 22-temp-cgi.c
@@ -1,37 +1,99 @@
 #include <unistd.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
+static int __argc__;
+static char** __argv__;
+
+static void sql_select_title(FILE* ff, const char* title)
+{
+    fprintf(ff, "select \"\n- %s՝\";\n", title);
+}
+
+static void sql_global_min(FILE* ff)
+{
+    sql_select_title(ff, "Բոլոր չափումների ամենացուրտը");
+    const char sql[] = "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select min(value) from data) order by time desc;\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void sql_global_max(FILE* ff)
+{
+    sql_select_title(ff, "Բոլոր չափումների ամենատաքը");
+    const char sql[] = "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select max(value) from data) order by time desc;\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void sql_24_hour_min(FILE* ff)
+{
+    sql_select_title(ff, "Վերջին 24 ժամի ամենացուրտը");
+    const char sql[] = "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select min(value) from data where time > datetime(\'now\', \'localtime\', \'-1 day\')) and time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void sql_24_hour_max(FILE* ff)
+{
+    sql_select_title(ff, "Վերջին 24 ժամի ամենատաքը");
+    const char sql[] = "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select max(value) from data where time > datetime(\'now\', \'localtime\', \'-1 day\')) and time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void sql_24_hour_negative(FILE* ff)
+{
+    sql_select_title(ff, "Վերջին 24 ժամի 0-ից ցածրները");
+    const char sql[] = "select \"-- \"||time||\"  \"||value||\"\" from data where value < 0 and time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void sql_24_hour_all(FILE* ff)
+{
+    sql_select_title(ff, "Վերջին 24 ժամի բոլոր չափումները");
+    const char sql[] = "select \"-- \"||time||\"  \"||value||\"\" from data where time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void sql_quit(FILE* ff)
+{
+    const char sql[] = ".quit\n";
+    fwrite(sql, sizeof(sql) - 1, 1, ff);
+}
+
+static void f_dump_general(FILE* ff)
+{
+    sql_global_min(ff);
+    sql_global_max(ff);
+    sql_24_hour_min(ff);
+    sql_24_hour_max(ff);
+    sql_24_hour_negative(ff);
+    sql_24_hour_all(ff);
+}
+
+static void prepare_sql(void (*proc)(FILE*))
 {
     int fds[2] = {0, 0};
-    char *const args[] = { "sqlite3", "-batch", "/home/pi/data/sensor.db", NULL };
-    const char cmd[] = "select \"Content-type: text/plain; charset=UTF-8\n\"\n;"
-
-    "select \"- Բոլոր չափումների ամենացուրտը՝\";\n"
-    "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select min(value) from data) order by time desc;\n"
-
-    "select \"\n- Բոլոր չափումների ամենատաքը՝\";\n"
-    "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select max(value) from data) order by time desc;\n"
-
-    "select \"\n- Վերջին 24 ժամի ամենացուրտը՝\";\n"
-    "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select min(value) from data where time > datetime(\'now\', \'localtime\', \'-1 day\')) and time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n"
-
-    "select \"\n- Վերջին 24 ժամի ամենատաքը՝\";\n"
-    "select \"-- \"||time||\"  \"||value||\"\" from data where value == (select max(value) from data where time > datetime(\'now\', \'localtime\', \'-1 day\')) and time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n"
-
-    "select \"\n- Վերջին 24 ժամի 0-ից ցածրները՝\";\n"
-    "select \"-- \"||time||\"  \"||value||\"\" from data where value < 0 and time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n"
-
-    "select \"\n- Վերջին 24 ժամի բոլոր չափումները՝\";\n"
-    "select \"-- \"||time||\"  \"||value||\"\" from data where time > datetime(\'now\', \'localtime\', \'-1 day\') order by time desc;\n"
-
-    ".quit\n";
-
     pipe(fds);
     close(STDIN_FILENO);
     dup2(fds[0], STDIN_FILENO);
-    write(fds[1], cmd, sizeof(cmd) - 1);
-    close(fds[1]);
-    execvp(args[0], args);
+    FILE* ff = fdopen(fds[1], "a");
+    proc(ff);
+    sql_quit(ff);
+    fclose(ff);
+}
+
+static void http_header()
+{
+    fprintf(stdout, "Content-type: text/plain; charset=UTF-8\n");
+    fflush(stdout);
+}
+
+int main(int argc, char **argv)
+{
+    __argc__ = argc;
+    __argv__ = argv;
+    char *const sql_args[] = { "sqlite3", "-batch", "/home/pi/data/sensor.db", NULL };
+    http_header();
+    prepare_sql(f_dump_general);
+    execvp(sql_args[0], sql_args);
 
     return 0;
 }
+
