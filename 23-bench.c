1f2e7d7f5c26d18efcb160511f1628926deb4a01
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2022-02-09 14:07:41 +0400

    Added benchmark test

---
+++ 23-bench.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
+#define PI              3.14159265358979323846
+
+static int run_under_clock(const char* label, void (*proc)())
+{
+    clock_t t1, t2;
+    printf("%s...", label);
+    t1 = clock();
+    proc();
+    t2 = clock();
+    printf("%fs\n", label, (double)(t2 - t1) / CLOCKS_PER_SEC);
+    return 0;
+}
+
+static uint64_t fib_uint64(uint64_t n) {
+  if (n <= 1) return n;
+  return fib_uint64(n - 1) + fib_uint64(n - 2);
+}
+
+static void bench_float()
+{
+    volatile float d1, d2;
+    float c, s;
+    const float f = 1000.0 * 2 * PI / 96000;
+    uint32_t i = 10000000;
+    while(i --)
+    {
+        c += s * f;
+        s -= c * f;
+    }
+    d1 = c;
+    d2 = s;
+}
+
+static void bench_uint64_fibonacci()
+{
+    fib_uint64(36);
+}
+
+int main()
+{
+    run_under_clock("Floating point", bench_float);
+    run_under_clock("uin64, recursion", bench_uint64_fibonacci);
+    return 0;
+}
+
