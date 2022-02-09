7638affd351e7b96ba28a8d06c7dafc866ef917b
Artashes Shahinyan <artashes.shahinyan@gmail.com>
2022-02-09 14:58:47 +0400

    added double benchmark

--- 23-bench.c
+++ 23-bench.c
@@ -35,6 +35,21 @@ static void bench_float()
     d2 = s;
 }
 
+static void bench_double()
+{
+    volatile double d1, d2;
+    double c, s;
+    const double f = 1000.0 * 2 * PI / 96000;
+    uint32_t i = 10000000;
+    while(i --)
+    {
+        c += s * f;
+        s -= c * f;
+    }
+    d1 = c;
+    d2 = s;
+}
+
 static void bench_uint64_fibonacci()
 {
     fib_uint64(36);
@@ -42,7 +57,8 @@ static void bench_uint64_fibonacci()
 
 int main()
 {
-    run_under_clock("Floating point", bench_float);
+    run_under_clock("Float", bench_float);
+    run_under_clock("Double", bench_double);
     run_under_clock("uin64, recursion", bench_uint64_fibonacci);
     return 0;
 }
